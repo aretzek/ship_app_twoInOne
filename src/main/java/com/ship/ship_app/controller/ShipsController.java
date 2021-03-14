6c85dfa841f26f119faeb4c3e25dd3f8e8bc6fe4
aretzek <aretzek@gmail.com>
2021-03-14 20:13:50 +0100

    endpoints for allVessels added

--- src/main/java/com/ship/ship_app/controller/ShipsController.java
+++ src/main/java/com/ship/ship_app/controller/ShipsController.java
@@ -2,41 +2,63 @@ package com.ship.ship_app.controller;
 
 
 import com.ship.ship_app.model.Ship;
+import com.ship.ship_app.service.ShipManager;
 import com.ship.ship_app.service.ShipRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Service
 @Controller
 @RequestMapping()
 public class ShipsController {
 
     ShipRepository shipRepository;
+    ShipManager shipManager;
+
+
+
     //ShipRepositoryGdynia shipRepositoryGdynia;
 
-    public ShipsController(ShipRepository shipRepository) {
+    public ShipsController(ShipRepository shipRepository, ShipManager shipManager) {
         this.shipRepository = shipRepository;
+        this.shipManager = shipManager;
 
     }
 
     @GetMapping("/allShipsGdansk")
     @ResponseBody
-    public List<Ship> viewUnipilGdansk() {
-        List<Ship> allGdansk = shipRepository.findAll();
-        allGdansk.stream().filter(str -> str.getPort().contains("Gdansk")).collect(Collectors.toList());
-        return allGdansk;
+    public List<Ship> viewGdansk() {
+
+        List<Ship> allShips = shipManager.getLastUpdatedShipList();
+        allShips.stream().filter(str -> str.getPort().contains("Gdansk")).collect(Collectors.toList());
+        return allShips;
 
     }
 
     @GetMapping("/allShipsGdynia")
     @ResponseBody
-    public List<Ship> viewUnipilGdynia() {
-
-        return  shipRepository.findAll().stream().filter(str -> str.getPort().contains("Gdynia")).collect(Collectors.toList());
+    public List<Ship> viewGdynia() {
+        List<Ship> allShips = shipManager.getLastUpdatedShipList();
+        allShips.stream().filter(str -> str.getPort().contains("Gdynia")).collect(Collectors.toList());
+        return allShips;
+    }
+    @GetMapping("/allShipsSzczecin")
+    @ResponseBody
+    public List<Ship> viewSzczecin() {
+        List<Ship> allShips = shipManager.getLastUpdatedShipList();
+        allShips.stream().filter(str -> str.getPort().contains("Szczecin")).collect(Collectors.toList());
+        return allShips;
+    }
+    @GetMapping("/allShipsSwinoujscie")
+    @ResponseBody
+    public List<Ship> viewSwinoujscie() {
+        List<Ship> allShips = shipManager.getLastUpdatedShipList();
+        allShips.stream().filter(str -> str.getPort().contains("Swinoujscie")).collect(Collectors.toList());
+        return allShips;
     }
 //
 //    @GetMapping("/allShipsGdyniaByArrivalPlace")
