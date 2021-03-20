461bdc1cc240ea4deb7563a6a7d24fefc20b270a
Artur Z <blogownia@gmail.com>
2021-03-20 11:35:56 +0100

    Controller poprawiony

--- src/main/java/com/ship/ship_app/controller/ShipsController.java
+++ src/main/java/com/ship/ship_app/controller/ShipsController.java
@@ -1,9 +1,15 @@
 package com.ship.ship_app.controller;
 
 
+import com.ship.ship_app.Swinoujscie.model.ShipSwinoujscie;
+import com.ship.ship_app.Swinoujscie.service.ShipManagerSwinoujscie;
+import com.ship.ship_app.Szczecin.service.ShipManagerSzczecin;
+import com.ship.ship_app.Szczecin.service.ShipSzczecin;
 import com.ship.ship_app.gdansk.model.ShipGdansk;
+import com.ship.ship_app.gdansk.service.ShipManager;
 import com.ship.ship_app.gdansk.service.ShipRepository;
 import com.ship.ship_app.gdynia.model.ShipGdynia;
+import com.ship.ship_app.gdynia.service.ShipManagerGdynia;
 import com.ship.ship_app.gdynia.service.ShipRepositoryGdynia;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,40 +26,53 @@ import java.util.stream.Collectors;
 @RequestMapping()
 public class ShipsController {
 
-    ShipRepository shipRepository;
-    ShipRepositoryGdynia shipRepositoryGdynia;
+    ShipManager shipManager;
+    ShipManagerGdynia shipManagerGdynia;
+    ShipManagerSzczecin shipManagerSzczecin;
+    ShipManagerSwinoujscie shipManagerSwinoujscie;
 
-    public ShipsController(ShipRepository shipRepository, ShipRepositoryGdynia shipRepositoryGdynia) {
-        this.shipRepository = shipRepository;
-        this.shipRepositoryGdynia = shipRepositoryGdynia;
+    public ShipsController(ShipManager shipManager, ShipManagerGdynia shipManagerGdynia, ShipManagerSzczecin shipManagerSzczecin, ShipManagerSwinoujscie shipManagerSwinoujscie) {
+        this.shipManager = shipManager;
+        this.shipManagerGdynia = shipManagerGdynia;
+        this.shipManagerSzczecin = shipManagerSzczecin;
+        this.shipManagerSwinoujscie = shipManagerSwinoujscie;
     }
 
     @GetMapping("/allShipsGdansk")
     @ResponseBody
     public List<ShipGdansk> viewUnipilGdansk() {
-         return shipRepository.findAll();
+         return shipManager.getLastUpdatedShipGdanskList();
 
     }
 
     @GetMapping("/allShipsGdynia")
     @ResponseBody
-    public List<ShipGdynia> viewUnipilGdynia() {
-        return  shipRepositoryGdynia.findAll();
+    public List<ShipGdynia> shipsGdynia (){
+        return shipManagerGdynia.getLastUpdatedShipGdyniaList();
+
     }
 
-    @GetMapping("/allShipsGdyniaByArrivalPlace")
+    @GetMapping("/allShipsSzczecin")
     @ResponseBody
-    public List<ShipGdynia> shipsGdyniaSortedByBerth (){
-        List<ShipGdynia> allGdynia = shipRepositoryGdynia.findAll();
-        return allGdynia.stream().sorted(Comparator.comparing(ShipGdynia::getArrivalPlace)).collect(Collectors.toList());
+    public List<ShipSzczecin> viewUnipilSzczecin() {
+        return shipManagerSzczecin.getLastUpdatedShipGdyniaList();
     }
 
-    @GetMapping("/allShipsGdanskByArrivalPlace")
+    @GetMapping("/allShipsSwinoujscie")
     @ResponseBody
-    public List<ShipGdansk> shipGdansksSortedByBerth (){
-        return shipRepository.findAll().stream().sorted(Comparator.comparing(ShipGdansk::getArrivalPlace)).collect(Collectors.toList());
-
+    public List<ShipSwinoujscie> viewUnipilSwinoujscie() {
+        return shipManagerSwinoujscie.getLastUpdatedShipSwinoujscieList();
     }
+/*    @PostMapping("/insertToken")
+    public void addToken( @RequestBody Ship ship){
+        shipManager.insertTokenToFirebase(ship);
+    }
+    @PostMapping("/removeToken/{shipId}/{token}")
+
+    public void removeToken( @PathVariable String shipId, @PathVariable String token){
+        shipManager.removeTokenFromFirebase(shipId,token);
+    }*/
+
 
 
 }
